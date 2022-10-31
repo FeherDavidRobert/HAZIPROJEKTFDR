6d1461d43311f6f6909ab88c2a1be3a9df1c8c65
Fehér Dávid Róbert <feherdavid429@gmail.com>
2022-10-31 15:23:10 +0100

    migráció és adatbázis

--- app/Http/Controllers/főoldal.php
+++ app/Http/Controllers/főoldal.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class főoldal extends Controller
 {
-    //
+    public function FŐOLDAL(){
+
+        return view("fooldal");
+    }
 }
